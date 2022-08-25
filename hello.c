e48f3fd4146311483529471f000dc5ff3dde50d2
alex herrera <aherrera14@huskers.unl.edu>
2022-08-25 21:00:08 +0000

    added major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Alex Herrera
+ *         EMAIL
+ * Date: 2022/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+printf("Alex Herrera\n");
+prinft("pre-engineering\n");
+  return 0;
+}
